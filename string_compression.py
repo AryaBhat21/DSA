66a77cbe4f77b2e683d0d45ffc0692b78a1fb89b
AryaBhat21 <arya21bhat05@gmail.com>
2026-06-04 09:56:18 +0530

    string_compression

--- string_compression.py
+++ string_compression.py
@@ -1,3 +1,16 @@
-s = aabbbcc
-
+#string compression: eg: s=aabbbcc ns=a2b3c2 if ns<s: print ns else print s
+s = input("Enter : ")
+ns = ""
 d = {}
+
+for i in s:
+    d[i] = d.get(i,0)+1
+
+for i in d:
+    ns += i + str(d[i])
+
+if len(ns)<len(s):
+    print(ns)
+
+else:
+    print(s)
